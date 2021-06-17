01bffcf115824418e80773e43f204b52a2da8ecd
João Pedro Prysthon <joaopedroprysthon@gmail.com>
2021-06-17 20:43:13 -0300

    feat: implent a list using react

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 
-function App() {
+function App() { 
+  const textInputRef = useRef<HTMLInputElement>(null)
+  const[list, setList] = useState<string[]>([]);
+
+  const addTask = () => { 
+    if (!textInputRef.current) return;
+    if (textInputRef.current.value === "") return;
+
+    let valorTexto = textInputRef.current.value;
+    textInputRef.current.value = "";
+    setList([...list, valorTexto]);
+  };
+function apagarItem(position: number) {
+  let listaFiltrada = list.filter((_, index) => index !== position)
+  setList(listaFiltrada)
+}
 
   return (
-    <div> </div>
+    <div> 
+      <input type="text" ref={textInputRef} />
+      <button onClick={addTask}> Add Task</button>
+
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>
+            <span>{item}</span>
+            <button onClick={() => apagarItem(index)}>Apagar</button>
+          </li>
+
+        ))}
+      </ul>
+    </div>
 
   );
 }
